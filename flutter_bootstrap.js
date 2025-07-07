40e44a62809ae7aaba29910701d5c658baac2788
JohnDen117 <reslyjohn@gmail.com>
2025-07-07 13:55:36 +0800

    V25.07.07: Updated UI, Added new download pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2060312552"
+    serviceWorkerVersion: "885493544"
   }
 });
